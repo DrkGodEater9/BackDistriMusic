c79fe5a42600ef31b6640ce01b35bc9370737710
DrkGodEater9 <alexander.moralesujueta@gmail.com>
2025-07-10 13:34:01 -0500

    algo

--- src/main/java/edu/progavud/distrimusic/music/DataInitializer.java
+++ src/main/java/edu/progavud/distrimusic/music/DataInitializer.java
@@ -18,65 +18,54 @@ public class DataInitializer implements CommandLineRunner {
     }
 
     private void insertarCanciones() {
-        crearCancion("Flowers", "Miley Cyrus", "Endless Summer Vacation", 
-            "https://i.scdn.co/image/ab67616d00001e02f4e47edfa135929c4cf8b85f");
+        crearCancion("Flowers", "Miley Cyrus", "Endless Summer Vacation",
+                "");
+        crearCancion("As It Was", "Harry Styles", "Harry's House",
+                "");
+        crearCancion("Shakira: Bzrp Music Sessions, Vol. 53", "Shakira", "BZRP Music Sessions",
+                "");
+        crearCancion("Unholy", "Sam Smith ft. Kim Petras", "Gloria",
+                "");
+        crearCancion("TQG", "Karol G ft. Shakira", "Mañana Será Bonito",
+                "");
+        crearCancion("La Botella", "Christan Nodal", "Outlaw",
+                "");
+        crearCancion("Tití Me Preguntó", "Bad Bunny", "Un Verano Sin Ti",
+                "");
+        crearCancion("Despechá", "Rosalía", "Motomami +",
+                "");
+        crearCancion("Enemy", "Imagine Dragons", "Mercury - Acts 1 & 2",
+                "");
+        crearCancion("Heat Waves", "Glass Animals", "Dreamland",
+                "");
+        crearCancion("Blinding Lights", "The Weeknd", "After Hours",
+                "");
+        crearCancion("Watermelon Sugar", "Harry Styles", "Fine Line",
+                "");
 
-        crearCancion("As It Was", "Harry Styles", "Harry's House", 
-            "https://i.scdn.co/image/ab67616d00001e022e8ed79e177ff6011076f5f0");
+        crearCancion("Levitating", "Dua Lipa", "Future Nostalgia",
+                "");
 
-        crearCancion("Shakira: Bzrp Music Sessions, Vol. 53", "Shakira", "BZRP Music Sessions", 
-            "https://i.scdn.co/image/ab67616d00001e02825ad71d587c8a86b40e2874");
+        crearCancion("Save Your Tears", "The Weeknd", "After Hours",
+                "");
 
-        crearCancion("Unholy", "Sam Smith ft. Kim Petras", "Gloria", 
-            "https://i.scdn.co/image/ab67616d00001e02c0235e5c7a0bf5ad81374e68");
+        crearCancion("Good 4 U", "Olivia Rodrigo", "SOUR",
+                "");
 
-        crearCancion("TQG", "Karol G ft. Shakira", "Mañana Será Bonito", 
-            "https://i.scdn.co/image/ab67616d00001e02df2d0539e43b91d992f0c2c3");
+        crearCancion("Industry Baby", "Lil Nas X ft. Jack Harlow", "MONTERO",
+                "");
 
-        crearCancion("La Botella", "Christan Nodal", "Outlaw", 
-            "https://i.scdn.co/image/ab67616d00001e02a46ced2bb6a5b9e4b72a2c9e");
+        crearCancion("Stay", "The Kid LAROI & Justin Bieber", "F*CK LOVE 3",
+                "");
 
-        crearCancion("Tití Me Preguntó", "Bad Bunny", "Un Verano Sin Ti", 
-            "https://i.scdn.co/image/ab67616d00001e02c0c267651c8d26cedc50a6be");
+        crearCancion("Anti-Hero", "Taylor Swift", "Midnights",
+                "");
 
-        crearCancion("Despechá", "Rosalía", "Motomami +", 
-            "https://i.scdn.co/image/ab67616d00001e02e85259a1cae29a8d91f2093d");
+        crearCancion("Bad Habit", "Steve Lacy", "Gemini Rights",
+                "");
 
-        crearCancion("Enemy", "Imagine Dragons", "Mercury - Acts 1 & 2", 
-            "https://i.scdn.co/image/ab67616d00001e027dd8f95320e8ef08aa121dfe");
-
-        crearCancion("Heat Waves", "Glass Animals", "Dreamland", 
-            "https://i.scdn.co/image/ab67616d00001e02e6732d25b2a8f2a7a22923d3");
-
-        crearCancion("Blinding Lights", "The Weeknd", "After Hours", 
-            "https://i.scdn.co/image/ab67616d00001e028863bc11d2aa12b54f5aeb36");
-
-        crearCancion("Watermelon Sugar", "Harry Styles", "Fine Line", 
-            "https://i.scdn.co/image/ab67616d00001e02adea3a07aba0a3627e71b0de");
-
-        crearCancion("Levitating", "Dua Lipa", "Future Nostalgia", 
-            "https://i.scdn.co/image/ab67616d00001e023ba82abb1a87bf65b1bb7bb5");
-
-        crearCancion("Save Your Tears", "The Weeknd", "After Hours", 
-            "https://i.scdn.co/image/ab67616d00001e028863bc11d2aa12b54f5aeb36");
-
-        crearCancion("Good 4 U", "Olivia Rodrigo", "SOUR", 
-            "https://i.scdn.co/image/ab67616d00001e02a91c10fe9472d9bd89802e5a");
-
-        crearCancion("Industry Baby", "Lil Nas X ft. Jack Harlow", "MONTERO", 
-            "https://i.scdn.co/image/ab67616d00001e02be82673b5f79d9658ec0a9fd");
-
-        crearCancion("Stay", "The Kid LAROI & Justin Bieber", "F*CK LOVE 3", 
-            "https://i.scdn.co/image/ab67616d00001e022928b933eefd7a9e49aee307");
-
-        crearCancion("Anti-Hero", "Taylor Swift", "Midnights", 
-            "https://i.scdn.co/image/ab67616d00001e02bb54dde68cd23e2a268ae0f5");
-
-        crearCancion("Bad Habit", "Steve Lacy", "Gemini Rights", 
-            "https://i.scdn.co/image/ab67616d00001e02b85259a5d79a82d6e2f5bf95");
-
-        crearCancion("About Damn Time", "Lizzo", "Special", 
-            "https://i.scdn.co/image/ab67616d00001e02c536e4edb2de1c0bb42bbcaa");
+        crearCancion("About Damn Time", "Lizzo", "Special",
+                "");
     }
 
     private void crearCancion(String titulo, String artista, String album, String imageUrl) {
