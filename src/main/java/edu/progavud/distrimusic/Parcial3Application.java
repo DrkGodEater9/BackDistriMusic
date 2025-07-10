b057a0cbf6bcaf081e44488807caeda88c764661
DrkGodEater9 <alexander.moralesujueta@gmail.com>
2025-07-09 20:35:47 -0500

    cors en teoria ya

--- src/main/java/edu/progavud/distrimusic/Parcial3Application.java
+++ src/main/java/edu/progavud/distrimusic/Parcial3Application.java
@@ -2,6 +2,9 @@ package edu.progavud.distrimusic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Parcial3Application {
@@ -9,5 +12,18 @@ public class Parcial3Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Parcial3Application.class, args);
 	}
-
+@Bean
+    //metodo para configurar el acceso al proyecto desde otros dominio o puertos
+    //este metodo permite resolver el problema con las politicas CORS de los navegadores
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/*")                   // Aplica a todas las rutas, despues cada controller le digo cual es pa que sea bonito
+                .allowedOrigins("")                 // Permite cualquier origen, asi no nos complicamos en mirar que puerto trabaja el ide del Frontend jiji
+                .allowedMethods("GET", "POST", "PUT", "DELETE")  // Sólo estos métodos
+                .allowedHeaders("*");                // Permite cualquier encabezado
+            }
+        };
+    }
 }
